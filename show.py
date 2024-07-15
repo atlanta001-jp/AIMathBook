11601c84a9c775ca03c291cef54deb5d1dadabde
atlanta <atsushi.hamano111@gmail.com>
2024-07-15 22:54:54 +0900

    add today

--- show.py
+++ show.py
@@ -1,9 +1,9 @@
-#!usrbinenv python3
-# -- coding utf-8 --
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 
 import random
 
-with open(.hyakunin.txt, encoding=utf-8) as f
+with open("./hyakunin.txt", encoding="utf-8") as f:
     wakas = [s.strip() for s in f.readlines()]
 
-print(wakas[random.randrange(len(wakas))])
+print("¡“ú‚Ìˆê‹å" + wakas[random.randrange(len(wakas))])
